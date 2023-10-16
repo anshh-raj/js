9e142fb7c836a841e8daa566e7e5a9dca0bf22e8
anshh-raj <124145650+anshh-raj@users.noreply.github.com>
2023-10-16 10:55:50 +0000

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -33,6 +33,6 @@ console.log(myn1);
 console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
+const myn2 = myArr.splice(1, 3) // it changes the original array
 console.log("C ", myArr);
 console.log(myn2);
